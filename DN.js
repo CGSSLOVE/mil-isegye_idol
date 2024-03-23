5fb6d8b44a367434cc27d8c04908e62129fe603e
CGSSLOVE <94452637+CGSSLOVE@users.noreply.github.com>
2024-03-23 22:56:12 +0900

    Update DN.js

--- DN.js
+++ DN.js
@@ -1,22 +1,16 @@
 function daynight(e){
 		var va1=e.value;
 		var sector =document.querySelector('body').style;
-		var list1 = document.querySelectorAll('.li1');
+		
 		if(va1=="night"){
 			e.value="day";
 			sector.backgroundColor="black";
 			sector.color="white";
-			for(i=0;i<list1.length;i++){
-				list1[i].style.color="white";
-			}
 		}
 		else if(va1=="day"){
 			e.value="night";
 			sector.backgroundColor="white";
-			sector.color="black";
-			for(i=0;i<list1.length;i++){
-				list1[i].style.color="black";
-			}	
+			sector.color="black";	
 		}
 		else {
 			alert("오류");
